11d566766c2ad359c06740013fb381ba8f49e859
Ashwath0715 <aswathprakash.23@gmail.com>
2022-07-26 19:45:07 +0530

    Added Generic_Queue and Stack

---
+++ Day 11/Generic_Queue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Queue<string> q = new Queue<string>();
+	
+			q.Enqueue("India");
+			q.Enqueue("US");
+			q.Enqueue("Japan");
+			q.Enqueue("Germany");
+			q.Enqueue("Canada");
+
+
+			Console.WriteLine("The contents of Queue: ");
+			foreach (var element in q) 
+			{
+				Console.WriteLine(element);
+			}
+			
+			Console.WriteLine("Does queue contains?  " + q.Contains("US"));
+			
+			Console.WriteLine("Total elements: {0}", q.Count);
+
+	
+			Console.WriteLine("Removing the element = " + q.Dequeue());
+			Console.WriteLine("Removing the next element = " + q.Dequeue());
+
+
+			Console.WriteLine("Peeking at the element = " + q.Peek());
+
+
+			Console.WriteLine("Updated Queue: ");
+			foreach (var element in q)
+				Console.WriteLine(element);
+			Console.ReadLine();
+		}
+    }
+}
---
+++ Day 11/Generic_Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Stack<int> st = new Stack<int>();
+			
+			st.Push(9);
+			st.Push(10);
+			st.Push(50);
+			st.Push(33);
+			st.Push(14);
+			
+			Console.WriteLine("The contents of Stack: ");
+			foreach (int element in st) 
+			{ 
+				Console.WriteLine(element); 
+			}				
+			Console.WriteLine("--------------------------------");
+
+			Console.WriteLine(st.Contains(10));
+			Console.WriteLine(st.Contains(16));
+			Console.WriteLine("--------------------------------");
+
+			Console.WriteLine("Popping out the top element = " + st.Pop());
+			Console.WriteLine("Popping out the next element = " + st.Pop());
+			Console.WriteLine("--------------------------------");
+
+			Console.WriteLine("Peeking at the current top element = " + st.Peek());
+			Console.WriteLine("--------------------------------");
+
+			Console.WriteLine("After Updating: ");
+			foreach (int element in st)
+				Console.WriteLine(element);
+			Console.ReadLine();
+		}
+    }
+}
